78453bbd5f6c04fadc0cbbaef3fc6f2a563f0e64
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-10-13 14:49:18 +1030

    Refactor query modification logic in backend.php
    Removed duplicate debug log and ensured 'admin_filterStatus' is always cast to an array. Commented out the secondary query modification block for shop_order post type, likely to prevent unintended query changes or for future refactoring.

--- classes/backend.php
+++ classes/backend.php
@@ -209,8 +209,6 @@ class wdo_Backend {
 		if ( ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http" ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" === 'https://bites.barossafresh.com.au/wp-admin/admin.php?page=wc-orders' ) {
 			$this->plugin->debug('[action_parse_query] Conditions met. Modifying query.');
 
-			$this->plugin->debug('[action_parse_query] Conditions met. Modifying query.');
-
 			// Get the 'orderby' value from plugin settings
 			$orderby = $this->plugin->getOption('admin_orderby', 'date'); // Default to 'date' if not set
 
@@ -219,7 +217,7 @@ class wdo_Backend {
 			$query->set('order', 'ASC');
 
 			// Exclude selected statuses
-			$excluded_statuses = $this->plugin->getOption('admin_filterStatus', []);
+			$excluded_statuses = (array) $this->plugin->getOption('admin_filterStatus', []); // Ensure it's always an array
 			if (!empty($excluded_statuses)) {
 				$query->set('post_status', array_diff(array_keys(wc_get_order_statuses()), $excluded_statuses));
 			}
@@ -229,28 +227,28 @@ class wdo_Backend {
 		}
 		
 		
-		if (is_admin() && $query->is_main_query() && $pagenow == 'edit.php' && isset($query_vars['post_type']) && $query_vars['post_type'] === 'shop_order') {
-			$this->plugin->debug('[action_parse_query] Conditions met. Modifying query.');
+		// if (is_admin() && $query->is_main_query() && $pagenow == 'edit.php' && isset($query_vars['post_type']) && $query_vars['post_type'] === 'shop_order') {
+		// 	$this->plugin->debug('[action_parse_query] Conditions met. Modifying query.');
 
-			// Get the 'orderby' value from plugin settings
-			$orderby = $this->plugin->getOption('admin_orderby', 'date'); // Default to 'date' if not set
+		// 	// Get the 'orderby' value from plugin settings
+		// 	$orderby = $this->plugin->getOption('admin_orderby', 'date'); // Default to 'date' if not set
 
-			// Set order by the retrieved value in ascending order
-			$query->set('orderby', $orderby);
-			$query->set('order', 'ASC');
+		// 	// Set order by the retrieved value in ascending order
+		// 	$query->set('orderby', $orderby);
+		// 	$query->set('order', 'ASC');
 
-				// Exclude selected statuses
-				$excluded_statuses = $this->plugin->getOption('admin_filterStatus', []);
-				if (!empty($excluded_statuses)) {
-					$query->set('post_status', array_diff(array_keys(wc_get_order_statuses()), $excluded_statuses));
-				}
+		// 		// Exclude selected statuses
+		// 		$excluded_statuses = $this->plugin->getOption('admin_filterStatus', []);
+		// 		if (!empty($excluded_statuses)) {
+		// 			$query->set('post_status', array_diff(array_keys(wc_get_order_statuses()), $excluded_statuses));
+		// 		}
 
-			// Debug log to confirm query modification
-			$this->plugin->debug('[action_parse_query] Query modified. Orderby: ' . $orderby . ', Order: ASC, Excluded Statuses: ' . implode(', ', $excluded_statuses));
-		} else {
-			// Debug log if conditions are not met
-			$this->plugin->debug('[action_parse_query] Conditions not met. Query not modified.');
-		}
+		// 	// Debug log to confirm query modification
+		// 	$this->plugin->debug('[action_parse_query] Query modified. Orderby: ' . $orderby . ', Order: ASC, Excluded Statuses: ' . implode(', ', $excluded_statuses));
+		// } else {
+		// 	// Debug log if conditions are not met
+		// 	$this->plugin->debug('[action_parse_query] Conditions not met. Query not modified.');
+		// }
 	}
 
 	/**
