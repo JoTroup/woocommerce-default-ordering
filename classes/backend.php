0a90f0e70fc268f39f1d3a884ba4d332608c6910
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-10-15 09:48:51 +1030

    Add product filter and update order query handling
    Introduces a new filter to modify the post WHERE clause for custom product filtering in order lists. Refactors the order status hiding logic to adjust query arguments for ordering by ID, and adds hooks for improved WooCommerce order table integration.

--- classes/backend.php
+++ classes/backend.php
@@ -27,6 +27,10 @@ class wdo_Backend {
 
 		add_filter('woocommerce_orders_table_query_clauses',[$this, 'action_parse_query'], 25);
 
+		add_action('woocommerce_order_list_table_prepare_items_query_args', [$this, 'hide_orders_by_status_for_role'], 25);
+
+		add_filter('post_where', [$this, 'action_parse_query_cpt'], 25, 2);
+
 		add_action( 'current_screen', function( $screen ) {
 
 			$this->plugin->debug('[action_parse_query] Screen ID ' . $screen->id);
@@ -231,22 +235,45 @@ class wdo_Backend {
 		return $clauses;
 	}
 
+
+	public function action_parse_query_cpt {$where, $query} {
+		global $wpdb;
+	
+		if( $query->is_search() ) {
+			return $where;
+		}
+
+		$selected_product_id = isset( $_GET[ 'filter_by_product' ] ) && $_GET[ 'filter_by_product' ] ? absint( $_GET[ 'filter_by_product' ] ) : 0;
+		if( ! $selected_product_id ) {
+			return $where;
+		}
+
+		$where .=
+		"
+		AND {$selected_product_id}
+		IN (
+			SELECT itemmeta.meta_value
+			FROM {$wpdb->prefix}woocommerce_order_items as items
+			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as itemmeta
+			ON itemmeta.order_item_id = items.order_item_id
+			WHERE items.order_item_type = 'line_item'
+			AND itemmeta.meta_key = '_product_id'
+			AND {$wpdb->posts}.ID = items.order_id
+		)
+		";
+
+		return $where;
+	}
+
 	/**
 	 * Hide orders by status for specific roles.
 	 */
-	public function hide_orders_by_status_for_role($query) {
-		if (!is_admin() || !$query->is_main_query()) return;
+	public function hide_orders_by_status_for_role($query_args) {
+		// Modify the query to order by order ID
+		$query_args['orderby'] = 'ID';
+		$query_args['order'] = 'ASC';
 
-		global $pagenow;
-		if ($pagenow !== 'edit.php' || $query->get('post_type') !== 'shop_order') return;
-
-		// Get excluded statuses from admin settings
-		$excluded_statuses = $this->plugin->getOption('admin_filterStatus', []);
-
-		if (!empty($excluded_statuses)) {
-			// Modify query to exclude those statuses
-			$query->set('post_status', array_diff(array_keys(wc_get_order_statuses()), $excluded_statuses));
-		}
+		return $query_args;
 	}
 	
 }
