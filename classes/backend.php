c3136056fbec43022fffb074f20b3b28647ba352
JoTroup <97867298+JoTroup@users.noreply.github.com>
2025-11-28 11:25:45 +1030

    Add debug log for custom orderby in admin filter
    Inserted an error_log statement to log when a custom orderby is used in the hide_orders_by_status_for_role filter. This aids in debugging and tracking the use of custom orderby options.

--- classes/backend.php
+++ classes/backend.php
@@ -409,11 +409,8 @@ class wdo_Backend {
 				return $query_args; // Skip applying the filter if the role doesn't match
 			}
 
-			if (
-				isset($options['admin_orderby_custom']) &&
-				!empty($options['admin_orderby_custom']) &&
-				$options['admin_orderby'] === 'custom'
-			) {
+			if (isset($options['admin_orderby_custom']) && !empty($options['admin_orderby_custom']) && $options['admin_orderby'] === 'custom') {
+				error_log('[hide_orders_by_status_for_role] Using custom orderby: ' . $options['admin_orderby_custom']);
 				$orderby = $options['admin_orderby_custom'];
 				// DO NOT set $query_args['meta_key']
 				$query_args['orderby'] = 'none'; // We'll handle ordering manually
